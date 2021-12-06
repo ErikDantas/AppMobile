34a8a35dec9f623983bcc0c83128d1db4d4cd5ba
Erik Dantas <jose.erikdantas@outlook.com>
2021-12-06 13:19:35 -0300

    Ajustes Cardapio e Carrinho

--- src/telas/Cardapio.js
+++ src/telas/Cardapio.js
@@ -1,66 +1,101 @@
-import React, { useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { DataTable } from 'react-native-paper';
+import React, {useState, useEffect } from 'react';
+import { Component } from 'react';
+import {  View, StyleSheet, Dimensions } from 'react-native';
+import { Button, List, Snackbar } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
-export default function  Cardapio () {
+export default class  Cardapio extends Component {
 
-    const state = {
-        itens: []
+    state = {
+        itens: [],
+        visible: false,
+        textoSnack: '',
+        qtdecarrinho: 0
     }
+
   
-   function getItens(){
+   getItens = () => {
     var url = window.servidor + '/item/'
     fetch(url)
         .then(response => response.json())
         .then((data) =>{
-            state.itens = data;
-            console.log("DATA: ",state.itens);
+            this.setState({itens: data});
+            console.log("DATA: ",this.state.itens);
         })
    }
 
-   useEffect(() => {
-        getItens();
-    
-    }, []);
+   componentDidMount(){
+       this.getItens();
+   }
+
+   GetTipoIcone = (item) => {
+    if(item.tipoItem == "Alcoolica" || item.tipoItem=="NaoAlcoolica"){
+        return "beer"
+    }else{
+        return "food-steak"
+    }
+   }
+   
+   DoAdicionarItemCarrinho = (item) => {
+    const url = window.servidor + '/item/carrinho/add/'+item.id
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            this.setState({qtdecarrinho: data})
+            sessionStorage.setItem('qtdecarrinho',this.state.qtdecarrinho)
+            const texto = item.nome + ' adicionado ao carrinho.'
+            this.setState({visible: true, textoSnack: texto})
+        })
+      
+   }
+
+   onDismissSnackBar = () => {
+       this.setState({visible: false})
+   }
  
+    render(){
+        return(
+            <>
+                <SafeAreaView>
+                {this.state.itens && this.state.itens.map((item) => {
+                    return (
+                        <View style={ estilos.container }>
+                            
+                            <List.Item
+                                title={item.nome}
+                                description={"R$ "+item.valor}
+                                left={props => <List.Icon {...props} icon={this.GetTipoIcone(item)}/>}
+                                right={props => 
+                                    <Button Style={estilos.botao} 
+                                            labelStyle={estilos.botaoTexto}
+                                            icon="plus" 
+                                            mode='contained'
+                                            compact="true"
+                                            onPress={() => this.DoAdicionarItemCarrinho(item)}
+                                            >add</Button>}
+                            />
+                            
+                        </View>
+                    )})
+                }
+                    <Snackbar
+                        visible={this.state.visible}
+                        onDismiss={this.onDismissSnackBar}
+                        style={estilos.snackstyle}
+                        action={{
+                            label: 'Ok',
+                            onPress: () => {
+                                {this.onDismissSnackBar}
+                            },
+                        }}>
+                        {this.state.textoSnack}
+                    </Snackbar>
+                </SafeAreaView>
+            </>
+        )
+    }
 
-    return(
-        <>
-            <SafeAreaView>
-                <View style={ estilos.container }>
-                    <DataTable>
-                        <DataTable.Header>
-                            <DataTable.Title>Item</DataTable.Title>
-                            <DataTable.Title numeric>Valor</DataTable.Title>
-                            <DataTable.Title numeric>Ação</DataTable.Title>
-                        </DataTable.Header>
-                        {state.itens.map(item =>{
-                            <DataTable.Row>
-                                <DataTable.Cell>{item.nome}</DataTable.Cell>
-                                <DataTable.Cell>{item.valor}</DataTable.Cell>
-                                <DataTable.Cell>{item.tempoPreparo}</DataTable.Cell>
-                            </DataTable.Row>
-                        })}
-                        
-
-                        <DataTable.Row>
-                            <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-                            <DataTable.Cell numeric>237</DataTable.Cell>
-                            <DataTable.Cell numeric>8.0</DataTable.Cell>
-                        </DataTable.Row>
-
-                        
-                    </DataTable>
-
-                </View>
-            </SafeAreaView>
-        </>
-    )
-
-    
-    
 }
 
 
@@ -71,9 +106,24 @@ const estilos = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "space-between",
-        backgroundColor: "#ffffff",
+        backgroundColor: "white",
         padding: 20,
         margin: 10,
     },
-
+    botao: {
+        textAlign: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        backgroundColor:'#8B008B',
+    },
+    botaoTexto:{
+        textAlign: 'center',
+        fontSize: 17,
+    },
+    snackstyle: {
+        position: 'fixed',
+        bottom: 0,
+        backgroundColor:'#8B008B',
+        
+    }
 });
